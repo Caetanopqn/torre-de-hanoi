39e5cf16aa2c0f2cc3f57ba1cca7f95330910c04
Caetanopqn <115043671+Caetanopqn@users.noreply.github.com>
2026-08-11 19:10:40 -0300

    Create hanoi2.py

---
+++ hanoi2.py
@@ -0,0 +1,104 @@
+#torre de hanói
+
+haste01 = [4, 3, 2, 1]
+haste02 = []
+haste03 = []
+
+hasteLista = [haste01, haste02, haste03]
+
+contador = 0
+
+while True:
+
+    #mostrar no display
+    print("\n1 -", haste01)
+    print("2 -", haste02)
+    print("3 -", haste03)
+    print(f"\njogadas: {contador}/15")
+
+    print("")
+    print("//" * 20)
+
+    while True:
+
+        #determinar haste de saída
+        hasteSaida = input("\nDigite o número da haste de SAÍDA: ")
+
+        try:
+            hasteSaida = int(hasteSaida)
+
+            if not 1 <= hasteSaida <= 3:
+                print("\nInsira um valor entre 1 e 3")
+                continue
+
+        except:
+            print("\nValor inválido!!!")
+            continue
+
+
+        #determinar haste de entrada
+        hasteEntrada = input("\nDigite o número da haste de ENTRADA: ")
+
+        try:
+            hasteEntrada = int(hasteEntrada)
+
+            if not 1 <= hasteEntrada <= 3:
+                print("\nInsira um valor entre 1 e 3")
+                continue
+
+        except:
+            print("\nValor inválido!!!")
+            continue
+
+
+        #verificar se as hastes são diferentes
+        if hasteSaida == hasteEntrada:
+            print("\nA haste de saída e a haste de entrada devem ser diferentes!")
+            continue
+
+
+        #transformar os números nas próprias listas
+        hasteSaida = hasteLista[hasteSaida - 1]
+        hasteEntrada = hasteLista[hasteEntrada - 1]
+
+
+        #verificar se a haste de saída está vazia
+        if len(hasteSaida) == 0:
+            print("\nA haste de saída está vazia!")
+            continue
+
+
+        #identificar o disco que será movimentado
+        discoSaida = hasteSaida[-1]
+
+
+        #verificar se a haste de entrada está vazia
+        if len(hasteEntrada) == 0:
+
+            hasteEntrada.append(hasteSaida[-1])
+            hasteSaida.pop(-1)
+
+        #verificar se o disco é maior que o outro
+        elif discoSaida < hasteEntrada[-1]:
+
+            hasteEntrada.append(hasteSaida[-1])
+            hasteSaida.pop(-1)
+
+        #movimento inválido
+        else:
+
+            print("\nMovimento inválido!")
+            continue
+        
+        #adicionar no contador
+        contador += 1
+
+        break
+        
+    #vitória
+    if haste03 == [4, 3, 2, 1]:
+        if contador > 15:
+            print("Você conseguiu... mas a que custo???")
+        else:
+            print("Vitória!!! :)")
+        break

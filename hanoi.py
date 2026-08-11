7634d58a5234eb8784e3efe97b36d2f49e02b7ff
Caetanopqn <115043671+Caetanopqn@users.noreply.github.com>
2026-08-11 19:08:59 -0300

    mudanças
    Added a move counter and victory condition to the game.

--- hanoi.py
+++ hanoi.py
@@ -6,12 +6,15 @@ haste03 = []
 
 hasteLista = [haste01, haste02, haste03]
 
+contador = 0
+
 while True:
 
     #mostrar no display
     print("\n1 -", haste01)
     print("2 -", haste02)
     print("3 -", haste03)
+    print(f"\njogadas: {contador}/15")
 
     print("")
     print("//" * 20)
@@ -32,7 +35,6 @@ while True:
             print("\nValor inválido!!!")
             continue
 
-
         #determinar haste de entrada
         hasteEntrada = input("\nDigite o número da haste de ENTRADA: ")
 
@@ -47,27 +49,22 @@ while True:
             print("\nValor inválido!!!")
             continue
 
-
         #verificar se as hastes são diferentes
         if hasteSaida == hasteEntrada:
             print("\nA haste de saída e a haste de entrada devem ser diferentes!")
             continue
 
-
         #transformar os números nas próprias listas
         hasteSaida = hasteLista[hasteSaida - 1]
         hasteEntrada = hasteLista[hasteEntrada - 1]
-
 
         #verificar se a haste de saída está vazia
         if len(hasteSaida) == 0:
             print("\nA haste de saída está vazia!")
             continue
 
-
         #identificar o disco que será movimentado
         discoSaida = hasteSaida[-1]
-
 
         #verificar se a haste de entrada está vazia
         if len(hasteEntrada) == 0:
@@ -75,8 +72,7 @@ while True:
             hasteEntrada.append(hasteSaida[-1])
             hasteSaida.pop(-1)
 
-        #verificar se o disco pode ser colocado sobre o outro
-        elif discoSaida < hasteEntrada[-1]:
+        elif discoSaida < hasteEntrada[-1]:  #verificar se o disco é maior que o outro
 
             hasteEntrada.append(hasteSaida[-1])
             hasteSaida.pop(-1)
@@ -86,5 +82,16 @@ while True:
 
             print("\nMovimento inválido!")
             continue
+        
+        #adicionar no contador
+        contador += 1
 
+        break
+        
+    #vitória
+    if haste03 == [4, 3, 2, 1]:
+        if contador > 15:
+            print("Você conseguiu... mas a que custo???")
+        else:
+            print("Vitória!!! :)")
         break
